2758fa1408cdb7b810b0e761b211b6660cf146d8
manickavasagam s <manickavasagam.s@myntra.com>
2017-04-11 16:58:47 +0530

    Event Broker

---
+++ src/test/java/eventbroker/EventBrokerTest.java
@@ -0,0 +1,12 @@
+package eventbroker;
+
+
+import org.junit.Test;
+
+public class EventBrokerTest {
+    @Test
+    public void EventBrokerConstruction()
+    {
+        EventBroker.Instance
+    }
+}
